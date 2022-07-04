39f2d77e405c887df3bf28dcf093028582046775
Diego <diego.fernandez-valderrama@alumnos.uva.es>
2022-07-04 16:56:47 +0200

    Pseudofuncional SubCadenas

---
+++ src/problems/subcadenas/SubcadenaNode.java
@@ -0,0 +1,140 @@
+package problems.subcadenas;
+
+import problems.prefijos.PrefijoNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Nodo que contiene un valor booleano, y apunta a otros nodos mediante un <code>char</code>.<p>
+ * Para almacenar los otros nodos se basa en <code>HashMap</code>.
+ *
+ * @author Diego Fernandez de Valderrama
+ */
+public class SubcadenaNode {
+    private boolean check = false;
+    private final HashMap<Character, SubcadenaNode> hijos;
+
+    public SubcadenaNode(){
+        this.hijos = new HashMap<>();
+    }
+
+    /**
+     * Añade un hijo al Nodo, actual. De este ya estar añadido cambia su estado a <code>true</code>.<p>
+     * Implicando que tiene más de una aparición, y por lo tanto es una entrada válida.
+     * @param c Character a añadir como hijo del Nodo.
+     * @return El nodo hijo, ya existente o creado.
+     */
+    public SubcadenaNode addSon(char c){
+        SubcadenaNode son;
+        if(!hijos.containsKey(c)) {
+            son = new SubcadenaNode();
+            hijos.put(c, son);
+        }else {
+            son = hijos.get(c);
+            son.check = true;
+        }
+
+        return son;
+    }
+
+    private void addSonRecIni(char c, SubcadenaNode nextTree, int len){
+        hijos.get(c).addSonRec(nextTree.addSon(c));
+    }
+
+    private void addSonRec(SubcadenaNode nextTree){
+        for (Map.Entry<Character, SubcadenaNode> entry : hijos.entrySet()) {
+            Character key = entry.getKey();
+            System.out.println("\tSON: " + key);
+            hijos.get(key).addSonRec(nextTree.addSon(key));
+        }
+    }
+    private boolean hasValidSon(){
+        for (Map.Entry<Character, SubcadenaNode> entry : hijos.entrySet()) {
+            if (entry.getValue().check)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Dado un Nodo, obtiene todos sus posibles combinaciones de hijos.<p>
+     * Todas estas combinaciones cumplen la condición de que aparece más de una vez su prefijo.
+     * @return Vector de Prefijos.
+     */
+    public String[] nextTree(SubcadenaNode nuevoArbol){
+        String tmp = "";
+        return toArrayComplex(tmp, nuevoArbol, this).toArray(new String[0]);
+    }
+
+    /**
+     * Recorre el Arbol, hasta llegar a un Nodo sin hijos, o en el que todos los elementos son  <code>False</code>.
+     * @param tmp String acumulado del paso anterior
+     * @return Lista con los elementos encontrados.
+     */
+    private List<String> toArrayComplex(String tmp,SubcadenaNode nuevoArbol, SubcadenaNode og){
+        List<String> salidas = new ArrayList<>();
+        boolean valid = false;
+        if(hijos.isEmpty()){
+            salidas.add(tmp);
+            return salidas;
+        }
+        for (Map.Entry<Character, SubcadenaNode> entry : hijos.entrySet()) {
+            Character key = entry.getKey();
+            SubcadenaNode value = entry.getValue();
+            if (value.check) {
+                valid = true;
+                salidas.addAll(value.toArrayComplex(tmp + key,nuevoArbol,og));
+            }else{
+                System.out.println("(" + tmp + ")KEY: " + key);
+                if(tmp.length() == 1 && !hasValidSon()) {
+                    System.out.println("(" + tmp + ")KEYg: " + key);
+                    addSonRecIni(key, nuevoArbol.addSon(tmp.charAt(0)), tmp.length());
+                }else
+                    addSonRecIni(key,nuevoArbol, tmp.length());
+            }
+        }
+        if(!valid) {
+            if(tmp.length() != 1)
+                salidas.add(tmp);
+        }
+        return salidas;
+    }
+
+    /**
+     * Dado un Nodo, obtiene todos sus posibles combinaciones de hijos.<p>
+     * Todas estas combinaciones cumplen la condición de que aparece más de una vez su prefijo.
+     * @return Vector de Prefijos.
+     */
+    public String[] toArray(){
+        String tmp = "";
+        return toArrayComplex(tmp).toArray(new String[0]);
+    }
+
+    /**
+     * Recorre el Arbol, hasta llegar a un Nodo sin hijos, o en el que todos los elementos son  <code>False</code>.
+     * @param tmp String acumulado del paso anterior
+     * @return Lista con los elementos encontrados.
+     */
+    private List<String> toArrayComplex(String tmp){
+        List<String> salidas = new ArrayList<>();
+        boolean valid = false;
+        if(hijos.isEmpty()){
+            salidas.add(tmp);
+            return salidas;
+        }
+        for (Map.Entry<Character, SubcadenaNode> entry : hijos.entrySet()) {
+            Character key = entry.getKey();
+            SubcadenaNode value = entry.getValue();
+            if (value.check) {
+                valid = true;
+                salidas.addAll(value.toArrayComplex(tmp + key));
+            }
+        }
+        if(!valid)
+            salidas.add(tmp);
+        return salidas;
+    }
+}
